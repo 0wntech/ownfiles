01607d3c4ec8b439005e7440337adc6fbff0ab89
ludwigschubi <ludwigschubi1@gmail.com>
2019-09-12 18:54:56 +0200

    Add create file function + tests

--- test/create.test.js
+++ test/create.test.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect;
 const auth = require("solid-auth-cli");
 const rdf = require("rdflib");
+const ns = require("solid-namespace")(rdf);
 const FileClient = require("../lib/index.js");
 const config = require("./clientConfig.json");
 
@@ -29,6 +30,30 @@ describe("Create", function() {
       expect(() =>
         fileClient.createFolder(
           config.testFolder.replace("https://", "lala://")
+        )
+      ).to.throw(Error);
+    });
+  });
+
+  describe("createFile()", function() {
+    it("should create a turtle file at the specified url with the turtle contents", async function() {
+      const contents = [rdf.st("https://lalasepp1.solid.community/profile/card#me", ns.foaf("knows"), "https://ludwig.owntech.de/profile/card#me")];
+      fileClient.createFile(config.testFile, { contents: contents }).then(res => {
+        expect(res.status).to.equal(201);
+      });
+    });
+    it("should create a plaintext file at the specified url with the contents", async function() {
+      fileClient.createFile(config.testFile, { contentType: "text/plain", contents: "Hello I am a text file." }).then(res => {
+        expect(res.status).to.equal(201);
+      });
+    });
+    it("should not create a file if there is no specified url and throw an error instead", function() {
+      expect(() => fileClient.createFile()).to.throw(Error);
+    });
+    it("should not create a file if there is an invalid url and throw an error instead", function() {
+      expect(() =>
+        fileClient.createFile(
+          config.testFile.replace("https://", "lala://")
         )
       ).to.throw(Error);
     });
