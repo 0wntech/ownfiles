4577aec691dd795461e8c4da736252671c2d55f0
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-10-26 21:22:34 +0100

    Add default for indexing when creating file

--- src/create.ts
+++ src/create.ts
@@ -122,7 +122,7 @@ export const createFile = async function(
         const parsedUrl = url.parse(fileAddress);
         const rootUrl = `${parsedUrl.protocol}//${parsedUrl.host}`;
         await this.addToIndex({
-            url: rootUrl + location,
+            url: rootUrl + location ?? fileAddress,
             type: options.contentType ?? 'text/plain',
         });
     }
